aeb3c40ecd94544ca9d8676c1f99b8f09bacd6fa
AutoWSL <139699764+AutoWSL@users.noreply.github.com>
2023-02-19 12:59:59 +0000

    Release

--- deployment/cv/views.py
+++ deployment/cv/views.py
@@ -48,4 +48,5 @@ def classification(request):
         print('Predicted:', decode_predictions(preds, top=3)[0])
         pred = decode_predictions(preds, top=1)[0][0][1]
         #return render(request, 'cv/upload.html', {'uploaded_file_url': uploaded_file_url})
-        return render(request, 'cv/classification.html', {'ori
+        return render(request, 'cv/classification.html', {'original_img': img_file,
+                       
