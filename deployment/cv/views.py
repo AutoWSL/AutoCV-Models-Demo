aad26b0b2d38d8bd083f31b00bc889823fcbc3eb
AutoWSL <139699764+AutoWSL@users.noreply.github.com>
2023-03-11 15:47:03 +0000

    Added License

--- deployment/cv/views.py
+++ deployment/cv/views.py
@@ -62,4 +62,4 @@ def get_segmentation(img_file, model):
     input_image = Image.open(img_file)
     preprocess = T.Compose([
         T.ToTensor(),
-        T.Normalize(mean=[0.485, 0.4
+        T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225
