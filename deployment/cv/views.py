dc0bc4fd8eabc7b00ede5b05daeae04cea876165
AutoWSL <139699764+AutoWSL@users.noreply.github.com>
2023-03-11 22:49:16 +0000

    speedups

--- deployment/cv/views.py
+++ deployment/cv/views.py
@@ -62,4 +62,7 @@ def get_segmentation(img_file, model):
     input_image = Image.open(img_file)
     preprocess = T.Compose([
         T.ToTensor(),
-        T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225
+        T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ])
+
+    input_tensor = preproc
