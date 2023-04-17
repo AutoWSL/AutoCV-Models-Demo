e70a7c7528c308028efdca401c8283281c0327a2
AutoWSL <139699764+AutoWSL@users.noreply.github.com>
2023-04-17 07:56:17 +0000

    URL updates

--- deployment/cv/views.py
+++ deployment/cv/views.py
@@ -87,4 +87,7 @@ label_colors = np.array([(0, 0, 0),  # 0=background
               # 11=dining table, 12=dog, 13=horse, 14=motorbike, 15=person
               (192, 128, 0), (64, 0, 128), (192, 0, 128), (64, 128, 128), (192, 128, 128),
               # 16=potted plant, 17=sheep, 18=sofa, 19=train, 20=tv/monitor
-              (0, 64, 0), (128, 64, 0), (0, 192, 0), (128, 192, 0), (0, 64, 128
+              (0, 64, 0), (128, 64, 0), (0, 192, 0), (128, 192, 0), (0, 64, 128)])
+
+def seg2rgb(preds):
+    colors 
