3521a7cc43a1ed88c315fa6337f68d8935a817d7
AutoWSL <139699764+AutoWSL@users.noreply.github.com>
2023-07-02 07:24:57 +0000

    Bump version to 0.5.0.

--- deployment/cv/views.py
+++ deployment/cv/views.py
@@ -130,4 +130,5 @@ COCO_INSTANCE_CATEGORY_NAMES = [
     'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
     'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket',
     'bottle', 'N/A', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl',
-    'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza'
+    'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
+    'donut', 'cake', 'chair', '
