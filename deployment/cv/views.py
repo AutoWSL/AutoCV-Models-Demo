a510dd6b123d671eb508f5c72d0654adf0694122
AutoWSL <139699764+AutoWSL@users.noreply.github.com>
2023-08-27 10:22:15 +0000

    add javascript

--- deployment/cv/views.py
+++ deployment/cv/views.py
@@ -146,4 +146,4 @@ def get_prediction(img_path, threshold):
     pred_class = [COCO_INSTANCE_CATEGORY_NAMES[i] for i in list(pred[0]['labels'].numpy())] # Get the Prediction Score
     pred_boxes = [[(i[0], i[1]), (i[2], i[3])] for i in list(pred[0]['boxes'].detach().numpy())] # Bounding boxes
     pred_score = list(pred[0]['scores'].detach().numpy())
-    pred_t = [pred_score.i
+    pred_t = [pred_score.index(x) for x in pred_score if
