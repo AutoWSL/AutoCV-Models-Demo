133b67c8db5f343d99c6d68e5860720db20f111c
AutoWSL <139699764+AutoWSL@users.noreply.github.com>
2023-11-08 10:27:27 +0000

    added toString()

--- deployment/cv/views.py
+++ deployment/cv/views.py
@@ -178,4 +178,6 @@ def object_detection(request):
         cv2.imwrite(obb_file, img)
 
         return render(request, 'cv/object_detection.html', {'original_img': img_file,
-                                                            'obb_img': '/medi
+                                                            'obb_img': '/media/obb_img.png'})
+        
+    return 
