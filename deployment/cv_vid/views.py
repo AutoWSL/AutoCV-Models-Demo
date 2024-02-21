8a7d61e86b3d42abe25cc118aa04d6fd2d06397b
AutoWSL <139699764+AutoWSL@users.noreply.github.com>
2024-02-21 08:46:20 +0000

    cov

---
+++ deployment/cv_vid/views.py
@@ -0,0 +1,35 @@
+
+from django.shortcuts import render, redirect
+
+# Create your views here.
+from django.http import HttpResponse
+from django.template import loader
+from django.core.files.storage import FileSystemStorage
+from django.conf import settings
+
+from tensorflow.keras.applications.vgg16 import VGG16
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.vgg16 import preprocess_input, decode_predictions
+
+import torch
+from torchvision import models
+import torchvision.transforms as T
+from PIL import Image
+import numpy as np
+import cv2
+from moviepy.editor import VideoFileClip
+
+def base(request):
+        
+    return render(request, 'cv_vid/base.html')        
+     
+
+def load_model():
+    # load the model for inference 
+    model = models.segmentation.fcn_resnet101(pretrained=True).eval()
+    return model
+
+def get_segmentation(input_image, model):
+    #input_image = Image.open(img_file)
+    preprocess = T.Compose([
+        T.ToTensor(),
