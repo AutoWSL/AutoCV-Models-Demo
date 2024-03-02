6f92399de3a37f62b6ebf0187b8b29d7366fed76
AutoWSL <139699764+AutoWSL@users.noreply.github.com>
2024-03-02 10:03:29 +0000

    fix wrong variable name

--- deployment/deployment/asgi.py
+++ deployment/deployment/asgi.py
@@ -3,4 +3,5 @@ ASGI config for deployment project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
-For more information on this file, s
+For more information on this file, see
+https://docs.djangoproject.com/en/3
