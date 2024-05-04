d8a9d55e38feefab605d0e566e050d207e219f89
AutoWSL <139699764+AutoWSL@users.noreply.github.com>
2024-05-04 20:37:46 +0000

    ready for testing

--- deployment/deployment/urls.py
+++ deployment/deployment/urls.py
@@ -23,4 +23,4 @@ urlpatterns = [
     path('cv_vid/', include('cv_vid.urls')),
     path('admin/', admin.site.urls),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.M
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
